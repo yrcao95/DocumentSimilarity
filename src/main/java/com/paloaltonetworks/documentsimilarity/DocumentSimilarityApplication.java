1b882acc70d6aa9c50fdbdaecbe7aecb455c701e
Yirui Cao <yiruicao@Yiruis-MacBook-Pro.local>
2021-07-08 00:14:00 -0500

    Init Commit

---
+++ src/main/java/com/paloaltonetworks/documentsimilarity/DocumentSimilarityApplication.java
@@ -0,0 +1,13 @@
+package com.paloaltonetworks.documentsimilarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentSimilarityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentSimilarityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paloaltonetworks/documentsimilarity/DocumentSimilarityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paloaltonetworks.documentsimilarity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentSimilarityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
